e8a30a26f425a93e5e86342a0d342c5e69ce66d0
w1zny <viznerjakub17@gmail.com>
2022-02-26 15:26:52 +0100

    started get-emails app

--- get_emails.py
+++ get_emails.py
@@ -1,7 +1,23 @@
+from readline import set_completion_display_matches_hook
 from openpyxl import Workbook
 from bs4 import BeautifulSoup
 import requests
 from get_restaurants import restaurant_urls
+import re
+
+#responses = []
+# for restaurant_url in restaurant_urls:
+#    response = requests.get(restaurant_url).text
+#    responses.append(response)
+response = requests.get(restaurant_urls[0]).text
+
+# for response in responses:
+doc = BeautifulSoup(response, "html.parser")
+sections = doc.find_all("section")
+for section in sections:
+    print(section.find_all("a"))
+    print("")
+    print("")
 
 # todo: z urlYelp ziskat urlRestaurant
 # todo: z urlRestaurant ziskat email
