e094a00d72c062d44a4bcfca10514c0ec520dd59
w1zny <viznerjakub17@gmail.com>
2022-02-26 08:15:02 +0100

    odskrtol som todo

--- get_emails.py
+++ get_emails.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 import requests
 from get_restaurants import restaurant_urls
 
-# todo general: pozri si yt tutorial na beautifull soup (kubo)
 # todo: zo urlYelp ziskat urlRestaurant
 # todo: z urlRestaurant ziskat email
 # todo: email ulozit do excelu
