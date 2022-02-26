eb954a830310acaec6861db25579b286745bca9a
w1zny <viznerjakub17@gmail.com>
2022-02-26 08:25:19 +0100

    zmenil som todos

--- get_emails.py
+++ get_emails.py
@@ -3,9 +3,7 @@ from bs4 import BeautifulSoup
 import requests
 from get_restaurants import restaurant_urls
 
-# todo general: pozri si yt tutorial na beautifull soup (kubo)
-# todo 1: brat urlYelp z excelu
-# todo 2: zo urlYelp ziskat urlRestaurant
-# todo 3: z urlRestaurant ziskat email
-# todo 4: email ulozit do excelu
+# todo: zo urlYelp ziskat urlRestaurant
+# todo: z urlRestaurant ziskat email
+# todo: email ulozit do excelu
 # DONE
