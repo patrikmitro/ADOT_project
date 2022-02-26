d6fdb97b8a758a06232dfac1fa503da249460863
w1zny <viznerjakub17@gmail.com>
2022-02-26 12:42:33 +0100

    opravil som typo v get-emails

--- get_emails.py
+++ get_emails.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import requests
 from get_restaurants import restaurant_urls
 
-# todo: zo urlYelp ziskat urlRestaurant
+# todo: z urlYelp ziskat urlRestaurant
 # todo: z urlRestaurant ziskat email
 # todo: email ulozit do excelu
 # DONE
